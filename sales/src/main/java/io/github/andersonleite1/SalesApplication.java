f574dc9ae035426ca737c998a91c8d9c7d8e8e4c
Anderson Leite <andersonleite.dev@gmail.com>
2023-07-14 16:29:14 -0300

    feat: hello world with Spring Boot

--- sales/src/main/java/io/github/andersonleite1/SalesApplication.java
+++ sales/src/main/java/io/github/andersonleite1/SalesApplication.java
@@ -2,9 +2,18 @@ package io.github.andersonleite1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SalesApplication {
+
+    @GetMapping("/hello")
+    public String hellorWorld() {
+        return "Hello world";
+    };
+
     public static void main(String[] args) {
         SpringApplication.run(SalesApplication.class, args);
     }
