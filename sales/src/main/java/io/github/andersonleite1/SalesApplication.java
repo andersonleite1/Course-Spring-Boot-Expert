0a5f08d9995a0f1a238de943c90fe8001b03bceb
Anderson Leite <andersonleite.dev@gmail.com>
2023-07-14 16:00:21 -0300

    feat: create class SalesApplication

---
+++ sales/src/main/java/io/github/andersonleite1/SalesApplication.java
@@ -0,0 +1,11 @@
+package io.github.andersonleite1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SalesApplication.class, args);
+    }
+}
