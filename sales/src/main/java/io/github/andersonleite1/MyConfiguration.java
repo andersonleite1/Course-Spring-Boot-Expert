673c7ef79a635ec04c6f4aad7f3de4ab797f2ef6
Anderson Leite <andersonleite.dev@gmail.com>
2023-07-15 11:10:11 -0300

    feat: clean project

--- sales/src/main/java/io/github/andersonleite1/MyConfiguration.java
+++
@@ -1,8 +0,0 @@
-package io.github.andersonleite1;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class MyConfiguration {
-}
