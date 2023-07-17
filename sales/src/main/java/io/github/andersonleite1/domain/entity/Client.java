2a858f80cc967f0e3026bbf039d18ed9e1d5302e
Anderson Leite <andersonleite.dev@gmail.com>
2023-07-17 09:03:40 -0300

    chore: create the model classes

---
+++ sales/src/main/java/io/github/andersonleite1/domain/entity/Client.java
@@ -0,0 +1,22 @@
+package io.github.andersonleite1.domain.entity;
+
+public class Client {
+    private Integer id;
+    private String nome;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
