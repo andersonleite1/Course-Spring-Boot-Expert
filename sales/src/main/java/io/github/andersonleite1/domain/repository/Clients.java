eacd79b446831bdc07b4574242ffaa3f446b1817
Anderson Leite <andersonleite.dev@gmail.com>
2023-07-18 11:54:35 -0300

    feat: add new queries at repositories Clients & Orders

--- sales/src/main/java/io/github/andersonleite1/domain/repository/Clients.java
+++ sales/src/main/java/io/github/andersonleite1/domain/repository/Clients.java
@@ -2,6 +2,7 @@ package io.github.andersonleite1.domain.repository;
 
 import io.github.andersonleite1.domain.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -10,8 +11,15 @@ import java.util.List;
 
 public interface Clients extends JpaRepository<Client, Integer> {
 
-    List<Client> findByNameLike(String name );
+    @Query(value = " select * from cliente c where c.nome like '%:nome%' ", nativeQuery = true)
+    List<Client> findByName(@Param("nome") String name);
 
-//    @Query(value = "select c from Client c where c.name like :name")
-//    List<Client> findByNameLike( @Param("name") String name );
+    @Query(" delete from Client c where c.name = :nome ")
+    @Modifying
+    void deleteByNome(String nome);
+
+    boolean existsByName(String name);
+
+    @Query(" select c from Client c left join fetch c.orders where c.id = :id  ")
+    Client findClientFetchOrders( @Param("id") Integer id );
 }
