1b4b3b706bd1d6d4dbecd1915c380397ced4c5d8
Anderson Leite <andersonleite.dev@gmail.com>
2024-03-12 06:23:55 -0300

    feat: implementation UserDetailService

--- sales/src/main/java/io/github/andersonleite1/configs/SecurityConfig.java
+++ sales/src/main/java/io/github/andersonleite1/configs/SecurityConfig.java
@@ -1,5 +1,7 @@
 package io.github.andersonleite1.configs;
 
+import io.github.andersonleite1.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -11,6 +13,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableAutoConfiguration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+  final private UserServiceImpl userService;
+
+  @Autowired
+  public  SecurityConfig(UserServiceImpl userService) {
+    this.userService = userService;
+  }
+
   @Bean
   public PasswordEncoder passwordEncoder(){
     return new BCryptPasswordEncoder();
@@ -18,11 +27,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.inMemoryAuthentication()
-        .passwordEncoder(passwordEncoder())
-        .withUser("client")
-        .password(passwordEncoder().encode("123"))
-        .roles("USER");
+    auth
+        .userDetailsService(userService)
+        .passwordEncoder(passwordEncoder());
   }
 
   @Override
---
+++ sales/src/main/java/io/github/andersonleite1/service/impl/UserServiceImpl.java
@@ -0,0 +1,35 @@
+package io.github.andersonleite1.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import sun.security.util.Password;
+
+@Service
+public class UserServiceImpl implements UserDetailsService {
+
+  final private PasswordEncoder passwordEncoder;
+
+  @Autowired
+  public UserServiceImpl(PasswordEncoder passwordEncoder) {
+    this.passwordEncoder = passwordEncoder;
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    if(!username.equals("John")){
+      throw new UsernameNotFoundException("User not found at database.");
+    }
+
+    return User
+        .builder()
+        .username("John")
+        .password(passwordEncoder.encode("123"))
+        .roles("USER", "ADMIN")
+        .build();
+  }
+}
